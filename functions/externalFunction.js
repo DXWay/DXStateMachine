c3969e672a42add192529e289dbb906d3062efbe
Faissal EL OSMAN <faissalelosman@Faissals-MacBook-Pro.local>
2021-08-27 22:30:17 +0400

    Modif

--- functions/externalFunction.js
+++ functions/externalFunction.js
@@ -2,7 +2,7 @@ const common = require('../generated/common');
 module.exports = {
     externalFunction : function(context) {
         return new Promise(function (resolve, reject) {
-            let outputString = 'externalFunction is called==============';
+            let outputString = 'externalFunction is called New one==============';
             context['EXTERN'] = 'Extern'
             console.log(context['USE_KEYBOARD_INPUT']);
             common.GOTO(context, 'START');   
